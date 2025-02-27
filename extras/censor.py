41b601e4012c3a697747faecbe13becfdf33d8b6
taps <71199052+tapsapt@users.noreply.github.com>
2025-02-27 01:53:59 -0600

    Update censor.py

--- extras/censor.py
+++ extras/censor.py
@@ -1,20 +1,20 @@
-import os
+#import os
 
-import numpy as np
-import torch
-from transformers import CLIPConfig, CLIPImageProcessor
+#import numpy as np
+#import torch
+#from transformers import CLIPConfig, CLIPImageProcessor
 
-import ldm_patched.modules.model_management as model_management
-import modules.config
-from extras.safety_checker.models.safety_checker import StableDiffusionSafetyChecker
-from ldm_patched.modules.model_patcher import ModelPatcher
+#import ldm_patched.modules.model_management as model_management
+#import modules.config
+#from extras.safety_checker.models.safety_checker import StableDiffusionSafetyChecker
+#from ldm_patched.modules.model_patcher import ModelPatcher
 
 safety_checker_repo_root = os.path.join(os.path.dirname(__file__), 'safety_checker')
 config_path = os.path.join(safety_checker_repo_root, "configs", "config.json")
 preprocessor_config_path = os.path.join(safety_checker_repo_root, "configs", "preprocessor_config.json")
 
 
-class Censor:
+#class Censor:
     def __init__(self):
         self.safety_checker_model: ModelPatcher | None = None
         self.clip_image_processor: CLIPImageProcessor | None = None
